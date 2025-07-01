7667fdcda8932f06705a851e95f836c76204d55d
MLSrinivas70 <80315375+MLSrinivas70@users.noreply.github.com>
2025-07-01 17:57:39 +0530

    Updated production url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  // baseURL: 'http://localhost:3000/api',
   // baseURL: 'https://fda-483-backend.onrender.com/api',
+  baseURL: 'https://fda483.orangeplant-b290c40b.centralus.azurecontainerapps.io/api',
   headers: {
     'Content-Type': 'application/json',
   },
